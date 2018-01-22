5c411dd1bd11dd406c0cc10fb44887d0d8aff17d
Yuma <ya7gisa0@gmail.com>
2018-01-22 11:55:59 +0900

    add an additional way to set waypoints

--- Dynamic Waypoints/Assets/Waypoint/WaypointFollow.cs
+++ Dynamic Waypoints/Assets/Waypoint/WaypointFollow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WaypointFollow : MonoBehaviour {
     public UnityStandardAssets.Utility.WaypointCircuit circuit;
+    //public GameObject[] waypoints;
     int currentWP = 0;
     public float speed = 30.0f;
     float accuracy = 0.1f;//1.0f;
@@ -11,30 +12,29 @@ public class WaypointFollow : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        // if you don't use standard assets
+        //waypoints = GameObject.FindGameObjectsWithTag("waypoint");
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
         if (circuit.Waypoints.Length == 0) return;
+        //if (waypoints.Length == 0) return;
 
 
-        Vector3 lookAtGoal = circuit.Waypoints[currentWP].position; //new Vector3(circuit.Waypoints[currentWP].position.x, this.transform.position.y,
-                                                                    //currentWP)
+        Vector3 lookAtGoal = circuit.Waypoints[currentWP].position;
+        //Vector3 lookAtGoal = waypoints[currentWP].transform.position;
         Vector3 direction = lookAtGoal - this.transform.position;
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
 
         if (direction.magnitude < accuracy)
         {
             currentWP++;
-            if (currentWP >= circuit.Waypoints.Length)
+            if (currentWP >= circuit.Waypoints.Length) //waypoints.Length)// circuit.Waypoints.Length)
             {
                 currentWP = 0;
             }
         }
         this.transform.Translate(0, 0, speed * Time.deltaTime);
-        {
-
-        }
 	}
 }
